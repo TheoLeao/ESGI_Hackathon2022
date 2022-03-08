912a70eb15c9b1a8391bac48394b5c097df4ccc7
ag7-alexis <guay.alexis7@gmail.com>
2022-03-08 20:20:45 +0100

    =)

---
+++ back/app/Http/Controllers/API/MetricsController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Session;
+use Illuminate\Http\Request;
+
+class MetricsController extends Controller
+{
+    public function getSessionMetrics(Session $session)
+    {
+        $countUsers = $session->usersSessions()->count();
+    }
+}
