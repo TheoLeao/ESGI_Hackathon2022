641cf788bb7f97cdd5b1f59c0bea50a347c66b60
mathieusi38 <mathieusi38@gmail.com>
2022-03-09 16:30:01 +0100

    modif user

--- front/pages/dashboard/user/index.jsx
+++ front/pages/dashboard/user/index.jsx
@@ -22,8 +22,8 @@ const User = ({ Component, pageProps }) => {
  
         useEffect(() => {
             var myHeaders = new Headers();
-            myHeaders.append("Authorization", "Bearer 2|4xJWQQi9RfNvZLU9s732d5CyATO2YZKxlbdQsDUc");
-            myHeaders.append("Cookie", "XSRF-TOKEN=eyJpdiI6IjZGR1hDazljNXBRTms0T1h0MXBnTlE9PSIsInZhbHVlIjoiTXBHUm43RHZZa2NSaUx5MGsydVN0dXhhUVQvOEdnRXI3ZVhQd01QUWRMUDRyc3BqWnZ3RGRSdXd3enZHM1FSeTJWZFBQZDVHZkZmMEVDMk8rVnFlazc0Z0h1VXJJVEdiU2hzMlJVb0MyWEl4K0tGVm0rZWF0YjNlUjFISTc4TzIiLCJtYWMiOiJkOTdlYWNhZDU2MGRiNjU3NjIyYzQxNjZmYmRmN2FiYzJlODc2MGUzOGMxNjE2MTliZTkxYjY2YzdlZmFiN2IxIiwidGFnIjoiIn0%3D; laravel_session=eyJpdiI6IjJ0ZHdNbHQ1QkdaZjY1Z0xtV2RCdWc9PSIsInZhbHVlIjoidkRtQTlndEJ3VUFKd3U3bUd4WkJGZXZWK1NsOE9qc0VUTnlHOHVEV2xWVzNqeHBTczVPU0ZCbjk1eStEU3AyUlJxQkp5L2psQmJuRXdGeGVDMjlkVzkxZmcwMzU5MXdubS9rTHYyZUVkdWhPN0J3a2VKRTdkUlZFL09ReXVxWmgiLCJtYWMiOiJlM2E5NGMwNmQ0NjgxZWMyNGZiMDBmMTNmNzg2NjBhZmJjOGU3NjFiNmJjMDhmMzM3OWEwMzc0Zjk5YjljNWM3IiwidGFnIjoiIn0%3D");
+            myHeaders.append("Authorization", "Bearer 3|3WAPauiyEbeYGr4FRpZg1hHtagkj1KmsIs4Q81ow");
+          
 
             var requestOptions = {
             method: 'GET',
@@ -34,17 +34,17 @@ const User = ({ Component, pageProps }) => {
             fetch("http://hackathon.alexis-guay.fr/api/users/2", requestOptions)
             .then(response => response.text())
             .then(function(result){
-                console.log(result)
+                console.log(result);
                setUsersList(JSON.parse(result));
              })
             .catch(error => console.log('error', error));
-        
         }, [])
+  
      
             return (
                <>
                <div className={`${styles.info} `}>
-                <Heading mb={10}> {user.name} Profile</Heading>
+                 <Heading mb={10}> {user.name} Profile</Heading>
                 <Table variant='simple' className={styles.table}>
                 <Tbody>
                     <Tr>
@@ -81,22 +81,22 @@ const User = ({ Component, pageProps }) => {
                 <Tbody>
                      <Tr>
                         <Td>street</Td>
-                        <Td>{user.address.street}</Td>
+                        <Td>{user?.address?.street}</Td>
                     </Tr>
                     <Tr>
                         <Td>city</Td>
-                        <Td>{user.address.city}</Td>
+                        <Td>{user?.address?.city}</Td>
                     </Tr>
                     <Tr>
                         <Td>zipcode</Td>
-                        <Td>{user.address.zipcode}</Td>
+                        <Td>{user?.address?.zipcode}</Td>
                     </Tr>
                     <Tr>
                         <Td>country</Td>
-                        <Td>{user.address.country}</Td>
+                        <Td>{user?.address?.country}</Td>
                     </Tr>
                 </Tbody>
-            </Table>
+            </Table> 
             </div>
 </>
             
