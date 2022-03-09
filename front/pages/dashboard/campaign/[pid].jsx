7fa4aa61ca284f0ceece9b1a8e808cf2cf9c4c93
Théo Boudier <theoleaoboudier@gmail.com>
2022-03-09 15:51:48 +0100

    finished state management, populated campaign and sessions data from store

--- front/pages/dashboard/campaign/[pid].jsx
+++ front/pages/dashboard/campaign/[pid].jsx
@@ -11,53 +11,59 @@ import {
     TableCaption,
     Text,
     Button, Heading, Container, Stack, Badge
-} from '@chakra-ui/react'
+} from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import { useSelector } from 'react-redux';
 
 const Campain = ({ Component, pageProps }) => {
-    let campain = {
-        "id": 1,
-        "name": "Campagne de test sur les crêmes pour la peau",
-        "description": "Cette campagne a été créee dans le but de tester scientifiquement les conséquences d'une application quotidienne des crêmes de peau",
-        "state": 0,
-        "products": [{
-            "id": 1,
-            "name": "Crême peau sensible",
-            "mark": "Nivea",
-            "code": "1234123F",
-            "category": "cream"
-        },
-        {
-            "id": 2,
-            "name": "Crême toutt ype de peau",
-            "mark": "Nivea",
-            "code": "1234123F",
-            "category": "cream"
-        }],
-        "sessions": [{
-            "id": 1,
-            "name": "Placebo",
-            "startDate": "01/02/22",
-            "endDate": "31/04/22"
-        },
-        {
-            "id": 2,
-            "name": "Produit véritable",
-            "startDate": "01/01/22",
-            "endDate": "31/09/22"
-        }
-        ]
-    };
+    const router = useRouter()
+    const { pid } = router.query;
+    // let campain = {
+    //     "id": 1,
+    //     "name": "Campagne de test sur les crêmes pour la peau",
+    //     "description": "Cette campagne a été créee dans le but de tester scientifiquement les conséquences d'une application quotidienne des crêmes de peau",
+    //     "state": 0,
+    //     "products": [{
+    //         "id": 1,
+    //         "name": "Crême peau sensible",
+    //         "mark": "Nivea",
+    //         "code": "1234123F",
+    //         "category": "cream"
+    //     },
+    //     {
+    //         "id": 2,
+    //         "name": "Crême toutt ype de peau",
+    //         "mark": "Nivea",
+    //         "code": "1234123F",
+    //         "category": "cream"
+    //     }],
+    //     "sessions": [{
+    //         "id": 1,
+    //         "name": "Placebo",
+    //         "startDate": "01/02/22",
+    //         "endDate": "31/04/22"
+    //     },
+    //     {
+    //         "id": 2,
+    //         "name": "Produit véritable",
+    //         "startDate": "01/01/22",
+    //         "endDate": "31/09/22"
+    //     }
+    //     ]
+    // };
+
+    const campaign = useSelector((state) => state.campaigns.find((campaign) => campaign.id == pid));
+    const sessions = useSelector((state) => state.campaigns.find((campaign) => campaign.id == pid)?.sessions);
     return (
         <>
-
-            <Heading as='h3' size='lg'>Détail de la campagne  {campain.state ? <Badge colorScheme='green'>Ouverte</Badge> : <Badge colorScheme='red'>Fermé</Badge>}</Heading>
+            <Heading as='h3' size='lg'>Détail de la campagne  {campaign?.state ? <Badge colorScheme='green'>Ouverte</Badge> : <Badge colorScheme='red'>Fermé</Badge>}</Heading>
 
 
             <div className={styles.content}>
                 <div className={`${styles.section} `}>
                     <Heading as='h4' size='md'>Description de la campagne</Heading>
                     <Container maxW='container.lg' className={styles.containerComponent}>
-                        {campain.description}
+                        {campaign?.description}
                     </Container>
                 </div>
                 <div className={`${styles.section} `}>
@@ -73,7 +79,7 @@ const Campain = ({ Component, pageProps }) => {
                                 </Tr>
                             </Thead>
                             <Tbody>
-                                {campain.products.map((product) => {
+                                {campaign?.products.map((product) => {
                                     return <Tr>
                                         <Td>{product.name}</Td>
                                         <Td>{product.mark}</Td>
@@ -92,16 +98,14 @@ const Campain = ({ Component, pageProps }) => {
                             <Thead>
                                 <Tr>
                                     <Th>Nom</Th>
-                                    <Th>Date de début</Th>
-                                    <Th>Date de fin</Th>
+                                    <Th>Description</Th>
                                 </Tr>
                             </Thead>
                             <Tbody>
-                                {campain.sessions.map((session) => {
+                                {campaign?.sessions.map((session) => {
                                     return <Tr>
-                                        <Td>{session.name}</Td>
-                                        <Td>{session.startDate}</Td>
-                                        <Td>{session.endDate}</Td>
+                                        <Td>{session.label}</Td>
+                                        <Td>{session.description}</Td>
                                     </Tr>
                                 })}
                             </Tbody>
--- front/pages/dashboard/campaign/index.jsx
+++ front/pages/dashboard/campaign/index.jsx
@@ -4,7 +4,7 @@ import styles from './index.module.scss';
 import { Container } from '@chakra-ui/react';
 import { Heading } from '@chakra-ui/react';
 import Link from 'next/link';
-import React from 'react';
+import React, {useState} from 'react';
 import DashboardLayout from '../../../src/layouts/DashboardLayout/DashboardLayout';
 import {
     Table, Thead, Tbody, Tfoot, Tr, Th, Td, TableCaption,
@@ -18,7 +18,10 @@ import { useFormik, Formik } from 'formik';
 import theme from '../../../src/theme/theme';
 import * as Yup from "yup";
 import { useSelector, useDispatch } from 'react-redux';
-import { addCampaign, removeCampaign, test } from '../../../src/store/features/campaign/campaignSlice';
+import { addCampaign, createSessionCampaign, removeCampaign } from '../../../src/store/features/campaign/campaignSlice';
+import Lottie from 'react-lottie';
+import loader from '../../../src/lotties/loader.json';
+
 const Modal_CreateCampaign = () => {
     const { isOpen, onOpen, onClose } = useDisclosure();
 
@@ -34,11 +37,11 @@ const Modal_CreateCampaign = () => {
     const dispatch = useDispatch();
     const formik = useFormik({
         initialValues: initialValues,
-        validationSchema:  validationSchema,
+        validationSchema: validationSchema,
         onSubmit: (values) => {
             alert(JSON.stringify(values, null, 2));
-            dispatch(addCampaign({id: 1, name: values.campaign_name, description: values.campaign_description}));
-            dispatch(removeCampaign({id: 1}));
+            dispatch(addCampaign({ id: 1, name: values.campaign_name, description: values.campaign_description, state: 1 }));
+            onClose();
         },
     })
     return (
@@ -81,31 +84,32 @@ const Modal_CreateCampaign = () => {
         </>
     )
 }
-const Modal_CreateSession= ({campaignId}) => {
+const Modal_CreateSession = ({ campaignId }) => {
     const { isOpen, onOpen, onClose } = useDisclosure();
 
     const initialRef = React.useRef()
     const initialValues = {
-        campaign_name: "",
-        campaign_description: "",
+        session_name: "",
+        session_description: ""
     };
+
     const validationSchema = Yup.object({
-        campaign_name: Yup.string().required('Le nom de la compagne est requis'),
-        campaign_description: Yup.string().required('La description de la campagne est requise')
+        session_name: Yup.string().required('Le nom de la session est requis'),
+        session_description: Yup.string().required('La description de la session est requise')
     });
     const dispatch = useDispatch();
     const formik = useFormik({
         initialValues: initialValues,
-        validationSchema:  validationSchema,
+        validationSchema: validationSchema,
         onSubmit: (values) => {
             alert(JSON.stringify(values, null, 2));
-            dispatch(addCampaign({id: 1, name: values.campaign_name, description: values.campaign_description}));
-            dispatch(removeCampaign({id: 1}));
+            dispatch(createSessionCampaign({ id: 1, label: values.session_name, description: values.session_description, idCampaign: campaignId }))
+            onClose();
         },
     })
     return (
         <>
-            <Button onClick={onOpen} colorScheme='teal' size='sm' leftIcon={<FiPlusCircle />}>Créer une campagne</Button>
+            <Button onClick={onOpen} colorScheme='teal' size='sm' leftIcon={<FiPlusCircle />}>Créer une session</Button>
             <Modal
                 initialFocusRef={initialRef}
                 isOpen={isOpen}
@@ -113,19 +117,19 @@ const Modal_CreateSession= ({campaignId}) => {
             >
                 <ModalOverlay />
                 <ModalContent>
-                    <ModalHeader>Créer une campagne</ModalHeader>
+                    <ModalHeader>Créer une session</ModalHeader>
                     <ModalCloseButton />
                     <VStack as="form" onSubmit={formik.handleSubmit}>
                         <ModalBody pb={6} w="100%">
                             <FormControl>
-                                <FormLabel>Nom de la campagne</FormLabel>
-                                <Input id="campaign_name" name="campaign_name" onChange={formik.handleChange} value={formik.values.campaign_name} ref={initialRef} placeholder='Nom de la campagne' />
-                                {formik.errors.campaign_name ? <Text fontSize='sm' color={theme.colors.danger.normal}>{formik.errors.campaign_name}</Text> : null}
+                                <FormLabel>Nom de la session</FormLabel>
+                                <Input id="session_name" name="session_name" onChange={formik.handleChange} value={formik.values.session_name} ref={initialRef} placeholder='Nom de la session' />
+                                {formik.errors.session_name ? <Text fontSize='sm' color={theme.colors.danger.normal}>{formik.errors.session_name}</Text> : null}
                             </FormControl>
                             <FormControl mt={4}>
-                                <FormLabel>Description de la campagne</FormLabel>
-                                <Textarea id="campaign_description" name="campaign_description" onChange={formik.handleChange} value={formik.values.campaign_description} placeholder='Description de la campagne' />
-                                {formik.errors.campaign_description ? <Text fontSize='sm' color={theme.colors.danger.normal}>{formik.errors.campaign_description}</Text> : null}
+                                <FormLabel>Description de la session</FormLabel>
+                                <Textarea id="session_description" name="session_description" onChange={formik.handleChange} value={formik.values.session_description} placeholder='Description de la session' />
+                                {formik.errors.session_description ? <Text fontSize='sm' color={theme.colors.danger.normal}>{formik.errors.session_description}</Text> : null}
                             </FormControl>
                         </ModalBody>
 
@@ -145,29 +149,21 @@ const Modal_CreateSession= ({campaignId}) => {
 }
 
 const Campaign = ({ Component, pageProps }) => {
-    let campaigns = [{
-        "id": 1,
-        "name": "Campagne de test sur les crêmes pour la peau",
-        "products": [{
-            "id": 1,
-            "name": "Crême peau sensible",
-            "mark": "Nivea",
-            "code": "1234123F",
-            "state": "comingsoon", //"coming-soon", "passed" ou "in-progress"
-            "category": "cream"
-        }],
-        "sessions": [{
-            "id": 1,
-            "label": "Placebo"
-        },
-        {
-            "id": 1,
-            "label": "Placebo"
-        }
-        ]
-    }];
+    const campaigns = useSelector((state) => state.campaigns);
     return (
-        <> 
+        <>
+            {/* { <Lottie
+                options={{
+                    loop: true,
+                    autoplay: true,
+                    animationData: loader,
+                    rendererSettings: {
+                        preserveAspectRatio: "xMidYMid slice"
+                    }
+                }}
+                height={200}
+                width={200}
+            />} */}
             <div className={styles.heading}>
                 <Heading as='h3' size='lg'>Les campagnes</Heading>
                 <Modal_CreateCampaign></Modal_CreateCampaign>
@@ -189,7 +185,7 @@ const Campaign = ({ Component, pageProps }) => {
                                     <Link href={'/dashboard/campaign/' + campaign.id}>
                                         <Button colorScheme='teal' size='sm'>Voir</Button>
                                     </Link>
-                                    <Button colorScheme='teal' size='sm'>Créer une session</Button>
+                                    <Modal_CreateSession campaignId={campaign.id}></Modal_CreateSession>
                                 </Stack>
                             </Td>
                         </Tr>
@@ -197,8 +193,6 @@ const Campaign = ({ Component, pageProps }) => {
                 </Tbody>
             </Table>
         </>
-
-
     )
 }
 
