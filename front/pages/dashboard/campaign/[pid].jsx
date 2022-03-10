2da9b52e656b664275d2ec1f6cc80a9ad2467ce4
ag7-alexis <guay.alexis7@gmail.com>
2022-03-10 20:58:03 +0100

    manage product picture

--- front/pages/dashboard/campaign/[pid].jsx
+++ front/pages/dashboard/campaign/[pid].jsx
@@ -1,5 +1,5 @@
-import styles from './index.module.scss';
-import DashboardLayout from '../../../src/layouts/DashboardLayout/DashboardLayout';
+import styles from "./index.module.scss";
+import DashboardLayout from "../../../src/layouts/DashboardLayout/DashboardLayout";
 import {
     Table,
     Thead,
@@ -10,19 +10,22 @@ import {
     Td,
     TableCaption,
     Text,
-    Button, Heading, Container, Stack, Badge
-} from '@chakra-ui/react';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { useSelector } from 'react-redux';
-import { useEffect, useState } from 'react';
-import { getCampaignById } from '../../../src/api/api';
-import useQuery from '../../../src/hooks/useQuery';
-
+    Button,
+    Heading,
+    Container,
+    Stack,
+    Badge,
+} from "@chakra-ui/react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { getCampaignById } from "../../../src/api/api";
+import useQuery from "../../../src/hooks/useQuery";
 
 const Campain = ({ Component, pageProps }) => {
     const role = null;
-    if (typeof window !== 'undefined') {
+    if (typeof window !== "undefined") {
         role = sessionStorage.getItem("role");
     }
     const query = useQuery();
@@ -37,18 +40,25 @@ const Campain = ({ Component, pageProps }) => {
     }, [query]);
     return (
         <>
-            <Heading as='h3' size='lg'>Détail de la campagne  {campaign?.state ? <Badge colorScheme='green'>Ouverte</Badge> : <Badge colorScheme='red'>Fermé</Badge>}</Heading>
+            <Heading as="h3" size="lg">
+                Détail de la campagne{" "}
+                {campaign?.state ? <Badge colorScheme="green">Ouverte</Badge> : <Badge colorScheme="red">Fermé</Badge>}
+            </Heading>
             <div className={styles.content}>
                 <div className={`${styles.section} `}>
-                    <Heading as='h4' size='md'>Description de la campagne</Heading>
-                    <Container maxW='container.lg' className={styles.containerComponent}>
+                    <Heading as="h4" size="md">
+                        Description de la campagne
+                    </Heading>
+                    <Container maxW="container.lg" className={styles.containerComponent}>
                         {campaign?.description}
                     </Container>
                 </div>
                 <div className={`${styles.section} `}>
-                    <Heading as='h4' size='md'>Produit: {campaign?.product?.name}</Heading>
-                    <Container maxW='container.lg' className={styles.containerComponent}>
-                        <Table variant='simple' className={styles.table}>
+                    <Heading as="h4" size="md">
+                        Produit: {campaign?.product?.name}
+                    </Heading>
+                    <Container maxW="container.lg" className={styles.containerComponent}>
+                        <Table variant="simple" className={styles.table}>
                             <Thead>
                                 <Tr>
                                     <Th>Marque</Th>
@@ -62,58 +72,66 @@ const Campain = ({ Component, pageProps }) => {
                                     <Td>{campaign?.product?.brand}</Td>
                                     <Td>{campaign?.product?.code_product}</Td>
                                     <Td>{campaign?.product?.category}</Td>
-                                    <Td></Td>
+                                    <Td>
+                                        <img src={campaign?.product?.picture} />
+                                    </Td>
                                 </Tr>
-
                             </Tbody>
                         </Table>
                     </Container>
                 </div>
-                {role != 'tester' &&
-                <div className={`${styles.section} `}>
-                    <Heading as='h4' size='md'>Liste des sessions</Heading>
-                    <Link href={'/dashboard/requests/' + campaign?.id}>
-                    <Button colorScheme='teal' size='sm' mt={3}>Demandes</Button>
-                    </Link>
-                    <Container maxW='container.lg' className={styles.containerComponent}>
-                        <Table variant='simple' className={styles.table}>
-                            <Thead>
-                                <Tr>
-                                    <Th>Nom</Th>
-                                    <Th>Description</Th>
-                                    <Th>Action</Th>
-                                </Tr>
-                            </Thead>
-                            <Tbody>
-                                {campaign?.sessions?.map((session) => {
-                                    return <Tr>
-                                        <Td>{session.name}</Td>
-                                        <Td>{session.description}</Td>
-                                        <Td>
-                                            <Link href={'/dashboard/resultQcm/' + session.id}>
-                                            <Button colorScheme='teal' size='sm'>Statistiques</Button>
-                                            </Link>
-                                            <Link href={'/dashboard/userSession/' + session.id}>
-                                            <Button ml={5} colorScheme='teal' size='sm'>Testeurs</Button>
-                                            </Link>
-                                        </Td>
+                {role != "tester" && (
+                    <div className={`${styles.section} `}>
+                        <Heading as="h4" size="md">
+                            Liste des sessions
+                        </Heading>
+                        <Link href={"/dashboard/requests/" + campaign?.id}>
+                            <Button colorScheme="teal" size="sm" mt={3}>
+                                Demandes
+                            </Button>
+                        </Link>
+                        <Container maxW="container.lg" className={styles.containerComponent}>
+                            <Table variant="simple" className={styles.table}>
+                                <Thead>
+                                    <Tr>
+                                        <Th>Nom</Th>
+                                        <Th>Description</Th>
+                                        <Th>Action</Th>
                                     </Tr>
-                                })}
-                            </Tbody>
-                        </Table>
-                    </Container>
-                </div>
-                }   
+                                </Thead>
+                                <Tbody>
+                                    {campaign?.sessions?.map((session) => {
+                                        return (
+                                            <Tr>
+                                                <Td>{session.name}</Td>
+                                                <Td>{session.description}</Td>
+                                                <Td>
+                                                    <Link href={"/dashboard/resultQcm/" + session.id}>
+                                                        <Button colorScheme="teal" size="sm">
+                                                            Statistiques
+                                                        </Button>
+                                                    </Link>
+                                                    <Link href={"/dashboard/userSession/" + session.id}>
+                                                        <Button ml={5} colorScheme="teal" size="sm">
+                                                            Testeurs
+                                                        </Button>
+                                                    </Link>
+                                                </Td>
+                                            </Tr>
+                                        );
+                                    })}
+                                </Tbody>
+                            </Table>
+                        </Container>
+                    </div>
+                )}
             </div>
-
         </>
-
-
-    )
-}
+    );
+};
 
 Campain.getLayout = function getLayout(Campain) {
-    return <DashboardLayout>{Campain}</DashboardLayout>
-}
+    return <DashboardLayout>{Campain}</DashboardLayout>;
+};
 
 export default Campain;
