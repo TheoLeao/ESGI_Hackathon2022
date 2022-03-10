6a1611db5e09bd9fb8b7d9dd6b9f0c071c5ca4fa
ag7-alexis <guay.alexis7@gmail.com>
2022-03-10 18:03:23 +0100

    aaa

--- back/app/Http/Controllers/API/SessionController.php
+++ back/app/Http/Controllers/API/SessionController.php
@@ -177,7 +177,7 @@ class SessionController extends Controller
         $userSession->user()->associate(User::find($attr['user_id']));
         try {
             $userSession->save();
-            $campaignRequest = CampaignRequest::join('sessions', 'campaign_requests.session_id', '=', 'sessions.id')->where('sessions.campaign_id', $session->campaign()->first()->id)->where('campaign_requests.user_id', $attr['user_id'])->first();
+            $campaignRequest = CampaignRequest::join('sessions', 'campaign_requests.session_id', '=', 'sessions.id')->where('sessions.campaign_id', $session->campaign_id)->where('campaign_requests.user_id', $attr['user_id'])->first();
             $campaignRequest->delete();
         } catch (QueryException $e) {
             $userSession = UserSession::where('user_id', $attr['user_id'])->where('session_id', $session->id)->first();
